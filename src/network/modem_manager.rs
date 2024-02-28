1bad0cbbc5215d616f7cc4b2be1246401d8d42a6
steven leadbeater <stevenleadbeater@live.co.uk>
2024-02-28 11:44:22 +0000

    Add device_identifier and device_id trying to find wwan name

--- src/network/modem_manager.rs
+++ src/network/modem_manager.rs
@@ -12,6 +12,7 @@ use mmdbus::modem::Modem as ModemAccess;
 use mmdbus::modem_signal::ModemSignal;
 use mmdbus::modem_modem3gpp::ModemModem3gpp;
 use mmdbus::sim::Sim as SimTrait;
+use nmdbus::device_modem::DeviceModem;
 
 const DBUS_NAME: &str = "org.freedesktop.ModemManager1";
 const DBUS_PATH: &str = "/org/freedesktop/ModemManager1";
@@ -95,6 +96,12 @@ impl Modem {
 	/// tag (e.g. binding the tag to a specific sysfs path).
 	pub fn device(&self) -> Result<String, Error> {
 		self.dbus.proxy(&self.path).device()
+	}
+	pub fn device_identifier(&self) -> Result<String, Error> {
+		self.dbus.proxy(&self.path).device_identifier()
+	}
+	pub fn device_id(&self) -> Result<String, Error> {
+		self.dbus.proxy(&self.path).device_id()
 	}
 
 	/// Overall state of the modem, given as a MMModemState value.
